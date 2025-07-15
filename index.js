f1dd717f826a8174208977214cf8efb7b62ca672
Héctor Aguilera <hectoraguiler@gmail.com>
2025-07-15 11:15:36 -0400

    fix: remove git commands from index.js

--- index.js
+++ index.js
@@ -52,6 +52,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-git add .
-git commit -m "fix: usa sandbox number en Twilio"
-git push
